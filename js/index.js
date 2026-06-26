c133b43362de9fc5b7f042c921ba636b178d7de9
krispaje <nork.kristina@gmail.com>
2026-06-26 17:37:19 +0300

    added current location

--- js/index.js
+++ js/index.js
@@ -23,6 +23,10 @@ function everySecond() {
 setInterval(everySecond, 1000);
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector(".cities");
